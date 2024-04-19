826df30d5a9e5f49be5e8bc571e0f5ef4315aac0
Gaurab-B <54475579+Gaurab-B@users.noreply.github.com>
2024-04-19 14:11:55 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,8 +1,8 @@
 import os
-import openai
+from openai import OpenAI
 
 # Temporarily set the environment variable in code
-os.environ['OPENAI_API_KEY'] = 'sk-proj-hAXw2glXDHUYNwpZYhv0T3BlbkFJSfwZNlnkJHXqroBKl7Mb'
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Get the API key from the environment variable
 api_key = os.getenv('OPENAI_API_KEY')
@@ -10,15 +10,15 @@ api_key = os.getenv('OPENAI_API_KEY')
 if api_key is None:
     raise ValueError("API key is not set. Please set the OPENAI_API_KEY environment variable.")
 
-# Set the API key for all requests
-openai.api_key = api_key
 
-# Set up the request to the API
-response = openai.Completion.create(
-    model="gpt-3.5-turbo", 
-    prompt="Translate the following English text to French: 'Hello, how are you?'", 
-    max_tokens=60
+client = OpenAI(api_key= api_key)
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
 )
 
-# Print out the response from the API
-print(response.choices[0].text.strip())
+print(completion.choices[0].message)

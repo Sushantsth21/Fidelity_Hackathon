c18a8c44fff0a82c90d824d85a955dd94e34f851
Gaurab-B <54475579+Gaurab-B@users.noreply.github.com>
2024-04-19 15:08:23 -0400

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,12 +1,24 @@
 import os
-import openai
+from dotenv import load_dotenv
+from openai import OpenAI
 
+load_dotenv()
+MY_ENV_VAR = os.getenv('OPENAI_API_KEY')
 # Ensure the OPENAI_API_KEY environment variable is set
-api_key = os.getenv('OPENAI_API_KEY')
+api_key = MY_ENV_VAR
 if api_key is None:
     raise ValueError("API key is not set. Please set the OPENAI_API_KEY environment variable.")
 
-# Configure the OpenAI library to use the API key
-openai.api_key = api_key
+#the api-key workds
 
-print(api_key)
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a knowledgeable assistant, skilled in explaining complex financial concepts with clarity and insight."},
+    {"role": "user", "content": "Can you explain the concept of compound interest in a simple and concise manner?"}
+  ]
+)
+
+print(completion.choices[0].message)

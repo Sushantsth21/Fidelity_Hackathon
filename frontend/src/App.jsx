dc4fff90d48293da2e3b9bc4e33f4b4d214031db
Sushant Shrestha <sushant.shrestha003@gmail.com>
2024-04-19 20:52:44 -0400

    completed the signin and signup pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import About from "./pages/About"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 export default function App() {
@@ -16,6 +17,7 @@ export default function App() {
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
